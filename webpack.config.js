6c3169bb9bc4a5a236cb553d2da82afd0e40c3c8
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2016-02-10 21:36:42 +0100

    Added stage-0 to babel config

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015', 'react', 'stage-0']
         }
       }
     ]
